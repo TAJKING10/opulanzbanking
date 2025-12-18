a07c1b5e3178c3a151e07f7b1b16111f25518214
Irvin Dulberg <contact@opulanz.com>
2025-12-18 10:55:29 +0000

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,8 @@ const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Only use static export for production builds, not dev server
-  ...(process.env.NODE_ENV === 'production' && {
-    //output: 'export',
-    basePath: '/opulanzbanking',
-    assetPrefix: '/opulanzbanking/',
-  }),
+  basePath: '/opulanzbanking',
+  assetPrefix: '/opulanzbanking/',
   images: {
     unoptimized: true,
     remotePatterns: [
